1656e9f3d196bac0c1894539bd2ef1746d6443f2
Massimiliano Stucchi <mstucchi@ripe.net>
2015-11-26 12:01:01 +0100

    Fix the call for zcat/gzcat.

--- route2mongo.sh
+++ route2mongo.sh
@@ -4,13 +4,13 @@
 
 wget "ftp://ftp.ripe.net/ripe/dbase/split/ripe.db.route.gz"
 
-zcat ripe.db.route.gz | awk 'BEGIN{ORS=""} $1 == "route:" {split($2, route, "/");print("db.routes.insert({ route : \"" route[1] "\", mask : " route[2] " , origin : ");} $1 == "origin:" {print("\"" $2 "\"});\n");}'  >> db.routes.js
+gzcat ripe.db.route.gz | awk 'BEGIN{ORS=""} $1 == "route:" {split($2, route, "/");print("db.routes.insert({ route : \"" route[1] "\", mask : " route[2] " , origin : ");} $1 == "origin:" {print("\"" $2 "\"});\n");}'  >> db.routes.js
 
 # fetch ripe.db.route6
 
 wget "ftp://ftp.ripe.net/ripe/dbase/split/ripe.db.route6.gz"
 
-zcat ripe.db.route6.gz | awk 'BEGIN{ORS=""} $1 == "route6:" {split($2, route, "/");print("db.routes6.insert ({ route : \"" route[1] "\", mask : " route[2] " , origin : ");} $1 == "origin:" {print("\"" $2 "\"});\n");}'  >> db.routes6.js
+gzcat ripe.db.route6.gz | awk 'BEGIN{ORS=""} $1 == "route6:" {split($2, route, "/");print("db.routes6.insert ({ route : \"" route[1] "\", mask : " route[2] " , origin : ");} $1 == "origin:" {print("\"" $2 "\"});\n");}'  >> db.routes6.js
 
 mongo routes --eval "db.routes.drop()"
 mongo routes < db.routes.js
